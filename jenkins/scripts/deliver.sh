d1a0e78801186505c2e6a541f4f61eed2212ad26
Agung Rachmad <103097069+agungrachmadprihanto@users.noreply.github.com>
2024-06-16 00:12:20 +0700

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env sh
+
+# Description of the build process
+echo 'The following "npm" command builds your Node.js/React application for'
+echo 'production in the local "build" directory (i.e. within the'
+echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
+echo 'correctly bundles React in production mode and optimizes the build for'
+echo 'the best performance.'
+
 # Enable debugging
 set -x
 
@@ -10,6 +19,16 @@ npm run build || { echo 'Build failed'; exit 1; }
 
 # Disable debugging
 set +x
+
+# Description of the start process
+echo 'The following "npm" command runs your Node.js/React application in'
+echo 'development mode and makes the application available for web browsing.'
+echo 'The "npm start" command has a trailing ampersand so that the command runs'
+echo 'as a background process (i.e. asynchronously). Otherwise, this command'
+echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
+echo 'is followed by another command that retrieves the process ID (PID) value'
+echo 'of the previously run process (i.e. "npm start") and writes this value to'
+echo 'the file ".pidfile".'
 
 # Enable debugging
 set -x
@@ -25,3 +44,9 @@ echo $! > .pidfile
 
 # Disable debugging
 set +x
+
+# Description of the application status
+echo 'Now...'
+echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
+echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
+echo 'created your initial Pipeline as a Jenkinsfile.)'
